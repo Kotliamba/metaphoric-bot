8e69992f3b32e2f63d176d7e87931cdbcd130699
Nick <80983073+Kotliamba@users.noreply.github.com>
2023-03-29 16:50:10 +0300

    test webhook

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,27 +1,39 @@
 import Vapor
 import TelegramVaporBot
 
+enum EndPoint {
+    case tgWebHook
+    
+    var description: String {
+        switch self {
+        case .tgWebHook:
+            return "telegramWebHook"
+        }
+    }
+}
+
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    
-    app.http.server.configuration.hostname = "195.133.48.73"
-    app.http.server.configuration.port = 80
+    guard
+        let apiKey = Environment.get("API_KEY"),
+        let hostName = Environment.get("HOST_NAME")
+    else {
+        return
+    }
     
     TGBot.log.logLevel = .error
     
-    if let apiKey = Environment.get("API_KEY") {
-        let bot = TGBot(app: app, botId: apiKey)
-        
-        let connection = TGLongPollingConnection(bot: bot)
-        
-        Task {
-            await DefaultBotHandlers.addHandlers(app: app, connection: connection)
-            try await connection.start()
-        }
+    let bot = TGBot(app: app, botId: apiKey)
+    let webHookURI = URI(string: hostName + "/" + EndPoint.tgWebHook.description)
+    let connection = TGWebHookConnection(bot: bot, webHookURL: webHookURI)
+    
+    Task {
+        await DefaultBotHandlers.addHandlers(app: app, connection: connection)
+        try await connection.start()
     }
     
-    try routes(app)
+    try routes(app, connection)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,27 @@
 import Vapor
+import TelegramVaporBot
 
-func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+func routes(_ app: Application, _ connection: TGWebHookConnection) throws {
+    try app.register(collection: TelegramController(connection: connection))
+}
+
+final class TelegramController: RouteCollection {
+    
+    private let connection: TGWebHookConnection
+    
+    init(connection: TGWebHookConnection) {
+        self.connection = connection
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    func boot(routes: Vapor.RoutesBuilder) throws {
+        routes.get(PathComponent(stringLiteral: EndPoint.tgWebHook.description), use: telegramWebHook)
+    }
+    
+    func telegramWebHook(_ req: Request) async throws -> Bool {
+        let update: TGUpdate = try req.content.decode(TGUpdate.self)
+        
+        try await connection.dispatcher.process([update])
+        
+        return true
     }
 }
